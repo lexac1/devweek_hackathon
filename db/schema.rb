586ae6a19933a3d233a9f67f03bb3b037230a182
tereg <teregalla@gmail.com>
2017-02-11 15:10:07 -0800

    Create category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211230830) do
+ActiveRecord::Schema.define(version: 20170211230938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_categories_on_picture_id", using: :btree
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "pic_url"
@@ -52,5 +60,6 @@ ActiveRecord::Schema.define(version: 20170211230830) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "categories", "pictures"
   add_foreign_key "pictures", "restaurants"
 end
