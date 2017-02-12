820fcbf4f38d9d1279a7aca19204ed6892e051ef
tereg <teregalla@gmail.com>
2017-02-11 18:00:40 -0800

    Create dislikes route for dislikes index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :pictures, only: [:show]
   end 
 
+  resources :dislikes, only: [:index] do 
+    resources :pictures, only: [:show]
+  end 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 end
